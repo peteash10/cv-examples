121bbdfeb6808b28f219a40848eba7abddd590e3
Pete Ashton <pete@peteashton.com>
2017-09-03 16:11:18 +0100

    Update sketch.js

--- FaceTracking/sketch.js
+++ FaceTracking/sketch.js
@@ -1,8 +1,8 @@
 // https://github.com/kylemcdonald/AppropriatingNewTechnologies/wiki/Week-2
 var capture;
 var tracker
-var w = 1620,
-    h = 960;
+var w = 960,
+    h = 600;
 
 function setup() {
     capture = createCapture(VIDEO);
