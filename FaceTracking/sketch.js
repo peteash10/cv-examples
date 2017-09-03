0bf7e1c7427dd77e0c063d37e68aa707b98a77b9
Pete Ashton <pete@peteashton.com>
2017-09-03 16:35:51 +0100

    .

--- FaceTracking/sketch.js
+++ FaceTracking/sketch.js
@@ -33,7 +33,7 @@ function draw() {
     for (var i = 0; i < positions.length; i++) {
    //     fill(map(i, 0, positions.length, 0, 360), 50, 100);
         fill(50, 50, 100);
-        ellipse(positions[i][0], positions[i][1], 10, 10);
+        ellipse(positions[i][0], positions[i][1], 5, 5);
       //  text(i, positions[i][0], positions[i][1]);
     }
 
